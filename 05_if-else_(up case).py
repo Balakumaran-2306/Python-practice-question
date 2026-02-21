6a324a4a15c5214f9f567181264eb4862560c349
Balakumaran <kumaranbala216@gmail.com>
2026-02-22 00:02:29 +0530

    Create 05_if-else_(up case).py

---
+++ 05_if-else_(up case).py
@@ -0,0 +1,15 @@
+#QUESTION--> WAP TO CHECK THE GIVEN CHARACTER IS UPPERCASE ALPHABET
+
+#ALGORITHM--->
+#step1:Get the inpt from the user
+#step2:check the character between A to Z using > and < operator.
+#step3:If condition satisfies print uppercase else print lowercase.
+
+
+
+#PROGRAM-->
+char=input('Enter the character:')
+if 'A'<=char<='Z':
+  print('uppercase alphabet')
+else:
+  print('lowercase alphabet')
