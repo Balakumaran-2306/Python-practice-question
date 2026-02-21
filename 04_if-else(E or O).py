af407927313ba15f384ef548e76b68c160f4fd2b
Balakumaran <kumaranbala216@gmail.com>
2026-02-21 23:56:00 +0530

    Update 04_if-else(E or O).py

--- 04_if-else(E or O).py
+++ 04_if-else(E or O).py
@@ -1,11 +1,11 @@
-#WAP TO CHECK THE GIVEN NUMBER IS EVEN OR ODD
+#QUESTION-->WAP TO CHECK THE GIVEN NUMBER IS EVEN OR ODD
 
-#ALGORITHM
+#ALGORITHM-->
 #step1:Get the input from the user as integer.
 #step2:Check the number using modulus operator and it is divisible or not.
 #step3:If divisible print even else print odd.
 
-#PROGRAM
+#PROGRAM-->
 num=int(input('Enter a number:'))
 if num%2==0:
   print('Even Number')
