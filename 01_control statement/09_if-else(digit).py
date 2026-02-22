ab0e6fd1a48d4e1cc3556ff46d6e8ff3956c7a02
Balakumaran <kumaranbala216@gmail.com>
2026-02-22 19:49:19 +0530

    Create 09_if-else(digit).py

---
+++ 01_control statement/09_if-else(digit).py
@@ -0,0 +1,14 @@
+#QUESTION-->WAP TO CHECK GIVEN CHARACTER IS DIGIT OR NOT
+
+#ALGORITHM-->
+#step1:Get the character from the user.
+#step2:check if the entered character is digit or not using operators.
+#step3:Else print it is not a digit.
+
+
+PROGRAM
+ch=input("Enter a character:"))
+if '0'<=ch<='9':
+  print("IT IS A DIGIT")
+else:
+  print("NOT A DIGIT")
