8846ed58ba69daba16877c9b36f5adc8af7c34fa
Balakumaran <kumaranbala216@gmail.com>
2026-02-22 18:19:22 +0530

    Create 06_if-else_(number).py

---
+++ 06_if-else_(number).py
@@ -0,0 +1,13 @@
+#QUESTION-->WAP TO CHECK THE GIVEN NUMBER IS POSITIVE OR NEGATIVE NUMBER
+
+#ALGORITHM-->
+#step1:Get the input from the user as integer.
+#step2:Check the number with two possiblity conditions
+#step3:If number is greater than or equal to zero print positive else print negative.
+
+PROGRAM-->
+num=int(input("Enter a number:"))
+if num>=0:
+  print("Positive number")
+else:
+  print("Negaative number")
