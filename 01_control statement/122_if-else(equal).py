295abe5024a9839b5e412e94ee4a3bd35cb7db68
Balakumaran <kumaranbala216@gmail.com>
2026-02-24 00:21:40 +0530

    Create 12_if-else(equal).py

---
+++ 01_control statement/122_if-else(equal).py
@@ -0,0 +1,16 @@
+#QUESTION--> WAP TO ACCEPT TWO INTEGERS AND CHECK WHETHER THEY ARE EQUAL OR NOT.
+
+#ALGORITHM-->
+#step1:Get the input as integer from the user.
+#step2:compare two values using (==).if it is true print equal.
+#step3:else print not equal.
+
+
+
+PROGRAM-->
+a=int(input("Enter number 1:"))
+b=int(input("Enter number 2:"))
+if a==b:
+  print("Both are equal")
+else:
+  print("NOT EQUAL")
