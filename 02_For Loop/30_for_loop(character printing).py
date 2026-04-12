3174bee2b999ea614ee6e26169be2c5934e5c3c4
Balakumaran <kumaranbala216@gmail.com>
2026-04-12 18:08:42 +0530

    Create 30_for_loop(character printing).py

---
+++ 02_For Loop/30_for_loop(character printing).py
@@ -0,0 +1,10 @@
+#Question-->WAP TO PRINT ALL CHARACTERS IN A GIVEN SSTRING
+
+#ALGORITHM-->
+STEP1-Fetch the character from the given string using for loop(variable name and collection)
+STEP2-And print the assigned character using  print statement
+
+#PROGRAM
+st='python'
+for char in st:
+  print(char)
