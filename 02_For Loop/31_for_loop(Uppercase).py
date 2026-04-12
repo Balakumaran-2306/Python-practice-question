8443aa24be32b5a31b9191a276bf9bb755574da1
Balakumaran <kumaranbala216@gmail.com>
2026-04-12 18:13:56 +0530

    Create 31_for_loop(Uppercase).py

---
+++ 02_For Loop/31_for_loop(Uppercase).py
@@ -0,0 +1,11 @@
+#QUESTION-->WAP TO PRINT ONLY UPPERCASE ALPHABET IN A GIVEN STRING
+
+#ALGORITHM-->
+STEP-1:Fetch the character using for loop. And check whether the character is uppercase or not.
+STEP-2:If it is uppercase print it using print statement.
+
+#PROGRAM
+st=input("Enter a string:")
+for char in st:
+  if char 'A' <=char<= 'Z':
+      print(char)
