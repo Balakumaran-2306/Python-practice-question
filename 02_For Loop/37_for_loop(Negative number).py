0ed6957ab542a15bdb87ae481293a635be975f2c
Balakumaran <kumaranbala216@gmail.com>
2026-04-12 21:05:51 +0530

    Update 37_for_loop(Negative number).py

--- 02_For Loop/37_for_loop(Negative number).py
+++ 02_For Loop/37_for_loop(Negative number).py
@@ -1,1 +1,12 @@
-#QUESTION-->
+#QUESTION-->WAP TO PRINT ALL THE POSITIVE NUMBERS IN A LIST
+
+#ALGORITHM-->
+STEP-1:Fetch the item from the list
+STEP-2:And check whether the number is less than or equal to 0.
+STEP-3:if it is greater print positive number
+
+#PROGRAM-->
+li=eval(input("Enter the list:"))
+for item in li:
+  if item<=0:
+    print(item)
