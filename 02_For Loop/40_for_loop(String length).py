37e7375b3ca04f02cf18389a476ffe94ca61c20d
Balakumaran <kumaranbala216@gmail.com>
2026-04-14 14:59:20 +0530

    Create 40_for_loop(String length).py

---
+++ 02_For Loop/40_for_loop(String length).py
@@ -0,0 +1,12 @@
+#QUESTION-->WAP TO COUNT THE LENGTH OF THE STRING WITHOUT USING LEN FUNCTION
+
+#ALGORITH-->
+STEP-1:Fetch the character from the string
+step-2:Count the value by incrementing the number
+
+#PROGRAM-->
+st=input("Enter a string:")
+out=0
+for char in st:
+  out+=1
+print(out)
