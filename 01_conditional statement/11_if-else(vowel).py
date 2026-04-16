3f7c874918400e749a03aee56c99636af056398e
Balakumaran <kumaranbala216@gmail.com>
2026-04-16 08:24:36 +0530

    Update 11_if-else(vowel).py

--- 01_conditional statement/11_if-else(vowel).py
+++ 01_conditional statement/11_if-else(vowel).py
@@ -1,1 +1,14 @@
-QUESTION
+#QUESTION-->WAP TO CHECK GIVEN CHARACTER IS VOWEL OR CONSONANT
+
+#ALGORITHM-->
+STEP-1:Get the input from the user as string and fetch the character.
+STEP-2:And check whether the character is present in vowel characters
+STEP-3:If it is not present print it is consonant.
+
+
+#PROGRAM-->
+ch=input("Enter a character:")
+if ch in 'AEIOUaeiou':
+    print("vowel")
+else:
+    print("consonant")
